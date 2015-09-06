e75a45375b570e7cbe6e1132061708968591b73d
engjasonp <engjasonp@gmail.com>
2015-09-05 21:56:19 -0700

    Initial Commit

---
+++ Swifty NinjaTests/Swifty_NinjaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swifty_NinjaTests.swift
+//  Swifty NinjaTests
+//
+//  Created by Jason Eng on 9/5/15.
+//  Copyright (c) 2015 EngJason. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Swifty_NinjaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
