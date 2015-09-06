203b94d7f29ece6c129771d0a98cae2e85851dab
engjasonp <engjasonp@gmail.com>
2015-09-05 22:14:54 -0700

    GameScene.swift: added bg image "sliceBackground" and modified gravity to dy: -6. Added gameScore label node and set var score to default at Int = 0. Added var lives to keep track of # of player lives

--- Swifty Ninja/GameScene.swift
+++ Swifty Ninja/GameScene.swift
@@ -9,8 +9,29 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var gameScore: SKLabelNode!
+    var score: Int = 0{
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
     override func didMoveToView(view: SKView) {
-
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        addChild(background)
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
